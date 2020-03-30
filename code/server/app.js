5abf280df32d6e9966a9552e825df045312b36fc
Rakesh Sherwal <Rakesh.Sherwal@XEBIAINDIA.LOCAL>
2020-03-30 23:24:12 +0530

    Set book scheme.

--- code/server/app.js
+++ code/server/app.js
@@ -2,14 +2,14 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config/config.env' });
-
 const app = express();
-app.use('/graphql', graphqlHTTP({
-
-}));
-
 //Load env vars.
 const PORT = process.env.PORT || 4000;
+
+dotenv.config({ path: './config/config.env' });
+
+app.use('/graphql', graphqlHTTP({
+  
+}));
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`));
---
+++ code/server/schema/schema.js
@@ -0,0 +1,32 @@
+const graphql = require('graphql');
+
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString }
+  })
+});
+
+// client call this query like book(id: '12'){
+// name
+// genre
+// }
+//args means in query id is neccessary.
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        // Code to get data from db/ other resources.
+      }
+    }
+  }
+})
